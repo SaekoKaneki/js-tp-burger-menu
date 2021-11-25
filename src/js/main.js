f3b52feb75ddb00474ea69d4b4cf22e0c3411c1a
Gwen <gwenaelle.batta@studetn.hepl.be>
2021-11-25 14:38:57 +0100

    init

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,14 @@
-document.documentElement.classList.add('js-enabled');
-document.querySelector('.nav-button').addEventListener('click',(event)=> {
-    const eMain =document.querySelector('main');
-    eMain.classList.toggle('is-opened');
-});
+(function (){
+    const oBurger ={
+        init(){
+            document.documentElement.classList.add('js-enabled');
+            document.querySelector('.nav-button').addEventListener('click',(event)=> {
+                this.eMain =document.querySelector('main');
+                this.eMain.classList.toggle('is-opened');
+            });
+        }
+    }
+    oBurger.init();
+})();
+
+
